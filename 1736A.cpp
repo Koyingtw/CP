550df092cb5bfdb399d6dfd686647f448b6a50b6
Koyingtw <koyingchen0523@gmail.com>
2022-10-11 07:26:15 +0800

    10/10 Desktop

--- 1736A.cpp
+++ 1736A.cpp
@@ -1,5 +1,3 @@
-#pragma region
-#pragma optimize("O3")
 #include <bits/stdc++.h>
 #define Weakoying ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
 #define int long long
@@ -15,58 +13,40 @@
 #define cmax(a, b) a = (a > b ? a : b)
 #define cmin(a, b) a = (a < b ? a : b)
 #define put(x) cout << x << endl;
-#define putarr(x) for(int i = 0; i < sizeof(x); i++) cout << x[i] << (" \n")[i == sizeof(x) - 1]; 
+#define DB(x) cerr << #x << " " << x << endl
+#define all(v) v.begin(), v.end()
 #define stop system("pause");
 #define MEM(x, n) memset(x, n, sizeof(x));
 #define lowbit(x) x &(-x)
 #if !LOCAL
 #define endl "\n"
+#pragma optimize("Ofast", "unroll-all-loops")
 #endif
-const int INF = 0x3f3f3f3f;
+const int INF = 0x3f3f3f3f3f3f3f3f;
 const int P = 1e9+7;
 
 using namespace std;
-#pragma endregion
 /******************************************************************************/
-#define MAXN 20
+#define MAXN 200005
 #define MAXM 1000005 
 int n, m;
-bool G[MAXN][MAXN];
+int x[]
 
-void sol()
-{
-	cin >> n;
-	for(int i = 0; i < n; i++)
-	{
-		for(int j = 0; j < n; j++)
-			cin >> G[i][j];
-	}
-	int ans = 0;
-	for(int mask = 0; mask < (1 << n); mask++)
-	{
-		int tmp = 0;
-		bool yes = 1;
-		for(int i = 0; i < n - 1; i++)
-		{
-			for(int j = i + 1; j < n; j++)
-			{
-				if(G[i][j] && (mask & (1 << i)) && (mask & (1 << j)))
-					yes = 0;
-			}
-		}
-		cmax(ans, (yes * (__builtin_popcount(mask))));
-	}
-	cout << ans << endl;
+void sol() {
+    cin >> n;
+    int sum = 0;
+    for (int i = 0, a; i < n; i++) {
+        cin >> a;
+
+    }
 }
 
-signed main()
-{
+signed main() {
     Weakoying;
     int t = 1;
-    //while (cin >> t)
-    {
-    	while (t--)
-        {
+    while (cin >> t)
+	{
+    	while (t--) {
             sol();
         }
     }
